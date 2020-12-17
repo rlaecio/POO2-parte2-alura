b560360f0e8833c13af1d8556f74869664c226e9
Gitlab.network <roque.souza@portodigital.pt>
2020-12-17 20:40:23 +0000

    Usando herança

--- src/Pessoa.php
+++ src/Pessoa.php
@@ -6,31 +6,31 @@ class Pessoa
     public string $nome;
     public CPF $cpf;
 
-    /**
-     * Pessoa constructor.
-     * @param string $nome
-     * @param CPF $cpf
-     */
+
     public function __construct(string $nome, CPF $cpf)
     {
         $this->nome = $nome;
         $this->cpf = $cpf;
     }
 
-    /**
-     * @return string
-     */
+
     public function recuperaNome(): string
     {
         return $this->nome;
     }
 
-    /**
-     * @return string
-     */
+
     public function recuperaCpf(): CPF
     {
         return $this->cpf;
+    }
+
+    public function validaNomeTitular(string $nomeTitular)
+    {
+        if (strlen($nomeTitular) < 5) {
+            echo "Nome precisa ter pelo menos 5 caracteres";
+            exit();
+        }
     }
 
 
--- src/Titular.php
+++ src/Titular.php
@@ -15,39 +15,9 @@ class Titular extends Pessoa
     }
 
 
-    /**
-     * @return string
-     */
-    public function recuperaCpf(): string
-    {
-        return $this->cpf->recuperaNumeroCpf();
-    }
-
-    /**
-     * @return string
-     */
-    public function recuperaNome(): string
-    {
-        return $this->nome;
-    }
-
-    private function validaNomeTitular(string $nomeTitular)
-    {
-        if (strlen($nomeTitular) < 5) {
-            echo "Nome precisa ter pelo menos 5 caracteres";
-            exit();
-        }
-    }
-
-    /**
-     * @return Endereco
-     */
     public function recuperaEndereco(): Endereco
     {
         return $this->endereco;
     }
-
-
-
 
 }
